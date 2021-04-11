d221a7263df8d9aee44b2264996eb8da1b7036e5
limin0428 <1690324671@qq.com>
2021-04-11 20:33:59 +0800

    chore: 修改文本缩进

--- code/js/publishSubscribe.js
+++ code/js/publishSubscribe.js
@@ -1,38 +1,38 @@
 class Event {
-    constructor() {
-        this._events = {}
+  constructor() {
+    this._events = {}
+  }
+  $on(eventName, event) {
+    if (this._events[eventName]) {
+      this._events[eventName].push(event)
+    } else {
+      this._events[eventName] = [event]
     }
-    $on (eventName, event) {
-        if (this._events[eventName]) {
-            this._events[eventName].push(event)
-        } else {
-            this._events[eventName] = [event]
-        }
-        return this
+    return this
+  }
+  $emit(eventName, data) {
+    if (this._events[eventName]) {
+      this._events[eventName].forEach(cb => cb(data))
     }
-    $emit (eventName, data) {
-        if (this._events[eventName]) {
-            this._events[eventName].forEach(cb => cb(data))
-        }
-        return this
+    return this
+  }
+  $remove(eventName, event) {
+    if (event) {
+      this._events[eventName].filter(cb => cb === event)
+    } else {
+      delete this._events[eventName]
     }
-    $remove (eventName, event) {
-        if (event) {
-            this._events[eventName].filter(cb => cb === event) 
-        } else {
-            delete this._events[eventName]
-        }
-    }
-    $clear () {
-        this._events = {}
-    }
+  }
+  $clear() {
+    this._events = {}
+  }
 }
 var s = new Event()
-s.$on('eat',eat).$on('play',play)
-function eat (food) {
-    console.log(food)
+s.$on('eat', eat).$on('play', play)
+function eat(food) {
+  console.log(food)
 }
-function play (toys) {
-    console.log(toys)
+function play(toys) {
+  console.log(toys)
 }
 s.$emit('play', '足球').$emit('eat', '西红柿')
